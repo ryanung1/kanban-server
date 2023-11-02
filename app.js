196304766f9cfa6cad269336086bcd7c5681a3f1
ryanung1 <ung.ryan1@gmail.com>
2023-11-02 22:28:21 +0000

    updated origin cors

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const boardRouter = require("./routers/boardRouter")
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true}));
+app.use(cors({ origin: "https://gleaming-dango-c3afd3.netlify.app/", credentials: true}));
 app.use(cookieParser());
 // app.use(cors({ origin: "http://localhost:5173", credentials: true}));
+https://gleaming-dango-c3afd3.netlify.app/
 app.use(express.json());
 
 
