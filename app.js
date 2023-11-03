10fb2bfaf84843032c0cb70922dac498f7dda5bb
ryanung1 <ung.ryan1@gmail.com>
2023-11-03 22:05:35 +0000

    Added dev server to cors origin

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const boardRouter = require("./routers/boardRouter")
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "https://kanban-client-xq93.onrender.com", credentials: true}));
+app.use(cors({ origin: ["https://kanban-client-xq93.onrender.com", "http://localhost:5173"], credentials: true}));
 app.use(cookieParser());
 // app.use(cors({ origin: "http://localhost:5173", credentials: true}));
 
